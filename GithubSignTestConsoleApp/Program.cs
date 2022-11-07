3bc831cbd321e62c759ec16e7ef7e378b25ee4a6
yejunhuang <79896273+yejunhuang@users.noreply.github.com>
2022-11-07 23:06:28 +0800

    添加项目文件。

---
+++ GithubSignTestConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GithubSignTestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World 2023!");
+        }
+    }
+}
